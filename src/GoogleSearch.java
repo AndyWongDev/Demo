e936781e3d2f3f0a405e0f977cb3397759d6ec56
Andy Wong <AndyWongDev@users.noreply.github.com>
2018-12-09 03:07:24 -0500

    Initial commit

---
+++ src/GoogleSearch.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GoogleSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Create Driver object
+		System.setProperty("webdriver.gecko.driver", "D:\\Coding\\Eclipse\\selenium-java\\geckodriver.exe");
+		
+		WebDriver driver=new FirefoxDriver();
+		
+		System.out.println("DEBUG: Script Start.");
+		
+		driver.get("http://google.com");
+		driver.findElement(By.name("q")).sendKeys("Hello World!");
+		//driver.findElement(By.name("q")).sendKeys("Hello World!"+Keys.RETURN);
+		
+		driver.findElement(By.cssSelector(".FPdoLc > center:nth-child(1) > input:nth-child(1)")).click();
+		//driver.findElement(By.cssSelector("input[type='submit']")); 	WRONG
+		//driver.findElement(By.xpath("//input[@name='btnK']"));		WRONG
+	}
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Selenium Code
+		
+		// Create Driver object
+		System.setProperty("webdriver.gecko.driver", "D:\\Coding\\Eclipse\\selenium-java\\geckodriver.exe");
+		
+		WebDriver driver=new FirefoxDriver();
+		
+		driver.get("http://google.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		
+		driver.get("http://yahoo.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		
+		driver.navigate().back();
+		System.out.println("Browser has clicked on the Back Button.");
+		driver.navigate().forward();
+		System.out.println("Browser has clicked on the Forward Button.");
+		driver.getPageSource();
+		driver.quit();
+		System.out.println("Selenium process has been ended.");
+	}
+}
